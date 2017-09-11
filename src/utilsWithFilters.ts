2aca55234270b8f57cdbc8168941819ae3c210f8
Tsigel <tsddaniil@gmail.com>
2017-09-11 11:05:53 +0300

    Add binary find and binary first find

--- src/utilsWithFilters.ts
+++ src/utilsWithFilters.ts
@@ -25,3 +25,49 @@ export function find<T extends object>(some: T[] | { [key: string]: T },
     }
     return result;
 }
+
+export function binaryFind<T extends object>(some: Array<T>, target: (data: T) => -1 | 0 | 1): T | void {
+    let result = null;
+
+    const step = function (arr: Array<T>): void {
+        const index = Math.round(arr.length / 2);
+        const item = arr[index];
+        switch (target(item)) {
+            case -1:
+                step(arr.slice(0, index));
+                break;
+            case 0:
+                result = item;
+                break;
+            case 1:
+                step(arr.slice(index));
+                break;
+        }
+    };
+
+    step(some.slice());
+
+    return result;
+}
+
+export function binaryFirstFind<T extends object>(some: Array<T>, target: (data: T) => boolean): void {
+    let result = null;
+
+    const step = function (arr: Array<T>): void {
+        const index = Math.round(arr.length / 2);
+        const item = arr[index];
+        if (target(item)) {
+            if (target(arr[index - 1])) {
+                step(arr.slice(0, index));
+            } else {
+                result = item;
+            }
+        } else {
+            step(arr.slice(index));
+        }
+    };
+
+    step(some.slice());
+
+    return result;
+}
